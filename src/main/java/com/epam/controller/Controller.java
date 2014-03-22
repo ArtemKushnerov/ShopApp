b2ee84ace6a2498470eea3006855bdd2ea9a4f77
ArtemKushnerov <kushnerovartem@gmail.com>
2014-03-23 00:11:18 +0300

    first shop app commit

---
+++ src/main/java/com/epam/controller/Controller.java
@@ -0,0 +1,27 @@
+package com.epam.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Artsiom_Kushniarou on 3/21/14.
+ */
+public class Controller extends HttpServlet {
+
+    private void processRequest(HttpServletRequest req, HttpServletResponse resp){
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
