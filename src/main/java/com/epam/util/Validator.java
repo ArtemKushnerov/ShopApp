42f6eecf2141a97f3dacabc42c6106606c299c86
ArtemKushnerov <kushnerovartem@gmail.com>
2014-04-01 20:39:24 +0300

    ss

--- src/main/java/com/epam/util/Validator.java
+++ src/main/java/com/epam/util/Validator.java
@@ -4,10 +4,45 @@ import java.util.Map;
 
 public class Validator {
 	public static boolean validate(String producer, String model, String color,
-			String dateOfIssue, String price, String notInStock,
-			Object errors) {
-		Map<String,String> err=(Map)errors;
-		err.put("sorry", "sorry");
-		return true;
+			String dateOfIssue, String price, String notInStock, Object errors) {
+		Map<String, String> err = (Map<String, String>) errors;
+		return validateProducer(producer, err) & validateColor(color, err)
+				& validateModel(model, err) & validatePrice(price, err)
+				& validateDate(dateOfIssue, err)
+				& validateNotInStock(notInStock, err);
 	}
+
+	private static boolean validateProducer(String producer,
+			Map<String, String> err) {
+		return isEmpty(producer);
+	}
+
+	private static boolean validateModel(String model, Map<String, String> err) {
+		return isEmpty(model)
+				&& model.matches("[a-zA-Z][a-zA-Z][0-9][0-9][0-9]");
+	}
+
+	private static boolean validateColor(String color, Map<String, String> err) {
+		return isEmpty(color);
+
+	}
+
+	private static boolean validateDate(String date, Map<String, String> err) {
+		return isEmpty(date) && date.matches("YYYY-MM-DD");
+	}
+
+	private static boolean validatePrice(String price, Map<String, String> err) {
+		return isEmpty(price)&& price.matches("[0-9]*\\.?[0-9]*");
+	}
+
+	private static boolean validateNotInStock(String notInStock,
+			Map<String, String> err) {
+		return Boolean.valueOf(notInStock);
+
+	}
+
+	static boolean isEmpty(String str) {
+		return str.length() == 0 || str == null;
+	}
+
 }
